ddfaaff6ca0297d9ac33e8f5fa7a22f578b92b7d
Arihant Upadhyay <arihant3435@gmail.com>
2024-09-06 04:30:21 +0530

    New change

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -42,10 +42,10 @@ const Weather = () => {
 
             const response=await fetch(url);
             const data=await response.json();
-            // if(!response.ok){
-            //     alert(data.message);
-            //     return;
-            // }
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
             console.log(data);
             const icon=allIcons[data.weather[0].icon] || clear_icon;
             setWeatherData({
